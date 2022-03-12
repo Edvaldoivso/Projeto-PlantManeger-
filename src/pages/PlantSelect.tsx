bcb68c0537700b16f91209ce5bec4fd5d749dbab
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2022-03-12 17:55:20 -0300

    Finalização do Plant Maneger

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,85 +1,89 @@
-import React from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    FlatList
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-} from 'react-native';
+import { Header } from "../components/Header";
+import { color } from "react-native-reanimated";
+import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
+import { EnviromentButton } from "../components/EnviromentButton";
+import api from "../services/api";
 
-import { Header } from '../components/Header';
-import { color } from 'react-native-reanimated';
-import colors from '../../styles/colors';
-import fonts from '../../styles/fonts';
-import { EnviromentButton } from '../components/EnviromentButton';
-
-
-export function PlantSelect() {
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-
-                <Header />
-
-                <Text style={styles.title}>
-                    Em qual ambiente
-                </Text>
-                <Text style={styles.subtitle}>
-                    você quer colocar sua planta?
-                </Text>
-
-
-            </View>
-            <View>
-
-
-                <FlatList
-                    data={[1, 2, 3, 4, 5,6,7]}
-
-                    renderItem={(item) =>
-                        <EnviromentButton
-                            title='Cozinha'
-                            active
-                        />
-                    }
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.enviromentList}
-                />
-
-            </View>
-
-        </View>
-
-    )
+interface EnviromentProps {
+  Key: string;
+  title: string;
 }
 
+export function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
 
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get("plants_environments");
+      setEnviroments([
+          {
+          key:'all',
+          title:'Todos',
+          },
+          ...data
+      ]);
+    }
+
+    fetchEnviroment();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
+      <View>
+        <FlatList
+          data={enviroments}
+          renderItem={({ item }) => (
+            <EnviromentButton 
+            title={item.title} 
+             />
+          )}
+
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.background,
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
 
-    },
-    title: {
-        fontSize: 17,
-        color: colors.heading,
-        fontFamily: fonts.heading,
-        lineHeight: 20,
-        marginTop: 15
-
-    },
-    subtitle: {
-        fontFamily: fonts.text,
-        fontSize: 17,
-        lineHeight: 20,
-        color: colors.heading
-
-
-    },
-    header: {
-        paddingHorizontal: 30
-    }
+  enviromentList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginRight: 32,
+    marginVertical: 32,
+  },
 });
